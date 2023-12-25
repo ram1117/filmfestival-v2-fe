804266a05adde28abeebae44795633b64aff247d
ram1117 <ramkumark1@outlook.com>
2023-12-25 13:01:07 +0530

    complete hero section in about page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 import MenuBar from "@/components/navbar/MenuBar";
 import NavBar from "@/components/navbar/NavBar";
 import MainFooter from "@/components/footer/MainFooter";
+import dynamic from "next/dynamic";
+
+const BreadCrumb = dynamic(() => import("@/components/breadcrumb/BreadCrumb"), {
+  ssr: false,
+});
 
 export const metadata: Metadata = {
   title: "Film Festival",
@@ -23,6 +28,10 @@ export default function RootLayout({
       <body className="font-roboto">
         <MenuBar />
         <NavBar />
+        <div className="hidden md:block absolute right-10 md:top-28 lg:top-32">
+          <BreadCrumb />
+        </div>
+
         {children}
         <MainFooter />
       </body>
